930549d0b3f1900abd62cbcab07f1a55fd503360
stonecode27 <20cestlavie1703@gmail.com>
2022-02-13 12:41:04 +0300

    fixed id substitution in weekly digest links

--- news/scheduler_jobs.py
+++ news/scheduler_jobs.py
@@ -9,7 +9,7 @@ def weekly_category_digest():
     weekly_posts = Post.objects.filter(creation_date__gte=(datetime.datetime.now() - datetime.timedelta(days=7)))
     for post in weekly_posts:
         for category in post.mtm_with_category.all():  # instance's all categories iteration
-            links_list = [f'http://127.0.0.1:8000/news/{key}' for key in weekly_posts.filter(mtm_with_category=category)]
+            links_list = [f'http://127.0.0.1:8000/news/{key.id}' for key in weekly_posts.filter(mtm_with_category=category)]
             links_string = '\n'.join(links_list)
             for sub in category.subscribers.all():  # category's all subs iteration
                 if sub:
